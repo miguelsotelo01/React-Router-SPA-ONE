af265073aed25d0d88df660391fe99452e4157c7
miguelsotelo01 <miky.a.sotelo.97@gmail.com>
2023-05-07 17:49:05 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
